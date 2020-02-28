a7c2b6d9112970c632922025b618dced7b54c017
Hades182 <43642010+Hades182@users.noreply.github.com>
2020-02-28 16:08:24 -0600

    navbar y maqueta
    t amo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,17 +6,135 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="{{url('css/index.css')}}">
 </head>
-<body>
-    <div class="contain">
+<body style="background-image: url('../img/fondo.png');
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    width: 100%;
+    height: 100%;
+    ">
+<nav class="navbar navbar-light bg-dark" style="position: fixed; top: 0; width: 100%; z-index: 1">
+  <a class="navbar-brand" id="txtlogo">
+    <span><img src="{{url('img/logo.png')}}" id="logo"></span>RedNeeko
+  </a>
+</nav>
+    <div class="container">
         <div class="row">
-            <div class="col-5">
-                
-            </div>
-            <div class="col-5">
-                
-            </div>
+           <div class="col-8" id="posteos">
+               <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+                <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+                <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+                <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+                <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+                <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+                <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+                <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+                <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+                <div class="card" id="post">
+                  <div class="card-header">
+                    Titulo
+                  </div>
+                  <div class="card-body">
+                    <blockquote class="blockquote mb-0">
+                      <p>Pus el contenido del post no?xd</p>
+                      <footer class="blockquote-footer"><cite title="Source Title">Usuario</cite></footer>
+                    </blockquote>
+                  </div>
+                </div>
+           </div>
+           <div class="col-3" id="tendencias">
+           </div>
         </div>
     </div>
 </body>

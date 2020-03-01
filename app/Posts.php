f3982ae6c2682be76ad72f1cd6044e53becbec17
Hades182 <43642010+Hades182@users.noreply.github.com>
2020-03-01 13:29:28 -0600

    xd

--- app/Posts.php
+++ app/Posts.php
@@ -9,8 +9,7 @@ class posts extends Model
 {
     public static function getPost(){
     	return  DB::table('post AS p')->leftjoin('likes AS l', 'l.id_post','p.id')
-    	->select('p.id','p.titulo','p.usuario','p.video','p.imagen','p.contenido','l.nlike','l.ndislike')
-    	->where('p.status',1)
+    	->select('p.id','p.titulo','p.usuario','p.video','p.imagen','p.contenido','l.nlike','l.ndislike','p.status')
     	->get();
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,10 +2,35 @@
 
 @section('contenido')
     <div class="container">
-        <div class="row">
+        <div class="row justify-content-center">
+          <div class="rounded col-8" id="bar">
+            <br>
+            <div class="form-row">
+              <div class="input-group col-5">
+                <div class="input-group-prepend">
+                  <label class="input-group-text" for="">Ver</label>
+                </div>
+                <select class="custom-select" id="">
+                  <option selected>Todos</option>
+                  <option value="1">Eliminados</option>
+                  <option value="2">De alta</option>
+                </select>
+              </div>
+              <div class="input-group col-4">
+                <div class="input-group-prepend">
+                  <div class="input-group-text">Buscar</div>
+                </div>
+                <input type="text" class="form-control" id="" placeholder="">
+              </div>
+              <div class="input-group col-3">
+                <a href="" class="btn btn-success btn-block">Agregar</a>
+              </div>
+            </div>
+            <br>
+          </div>
            <div class="col-8" id="posteos" >
                @foreach($posts as $p)
-                <div class="card">
+                <div class="card" id="post">
                   <div class="card-header text-center">
                     {{$p->titulo}} 
                   </div>
@@ -18,6 +43,11 @@
                       <a href="" class="btn btn-danger"><i class="fas fa-heart"></i> {{$p->nlike}} </a> 
                       <a href="" class="btn btn-secondary"><i class="fas fa-heart-broken"></i> {{$p->ndislike}}  </a>
                       <a href="" class="btn btn-light"><i class="fas fa-comment-dots"></i> Comentario(s)</a>
+                      @if($p->status == 1)
+                      <a href="" class="btn btn-danger"><i class="far fa-window-close"> Dar de baja post</i></a>
+                      @else
+                      <a href="" class="btn btn-success"><i class="fas fa-upload"></i> Resubir post</a>
+                      @endif
                     </blockquote>
                   </div>
                 </div>

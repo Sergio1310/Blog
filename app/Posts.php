4737e1248f91b99871f0af12ad82e4750e2fabf6
Hades182 <43642010+Hades182@users.noreply.github.com>
2020-03-01 12:35:42 -0600

    xd

--- app/Posts.php
+++ app/Posts.php
@@ -8,6 +8,9 @@ use DB;
 class posts extends Model
 {
     public static function getPost(){
-    	return  DB::table('post')->get();
+    	return  DB::table('post AS p')->leftjoin('likes AS l', 'l.id_post','p.id')
+    	->select('p.id','p.titulo','p.usuario','p.video','p.imagen','p.contenido','l.nlike','l.ndislike')
+    	->where('p.status',1)
+    	->get();
     }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,23 +5,23 @@
         <div class="row">
            <div class="col-8" id="posteos" >
                @foreach($posts as $p)
-                <div class="card" id="post">
+                <div class="card">
                   <div class="card-header text-center">
-                    {{$p->titulo}}
+                    {{$p->titulo}} 
                   </div>
                   <div class="card-body text_container" >
                     <blockquote class="blockquote mb-0">
                       <p>{{$p->contenido}}</p>
                       <footer class="blockquote-footer"><cite title="Source Title">{{$p->usuario}}</cite></footer>
+                      <br>
+                      <input type="hidden" value="{{$p->id}}">
+                      <a href="" class="btn btn-danger"><i class="fas fa-heart"></i> {{$p->nlike}} </a> 
+                      <a href="" class="btn btn-secondary"><i class="fas fa-heart-broken"></i> {{$p->ndislike}}  </a>
+                      <a href="" class="btn btn-light"><i class="fas fa-comment-dots"></i> Comentario(s)</a>
                     </blockquote>
-                  </div>
-                  <div class="card-footer text-muted">
-                    <a href="" class="btn btn-danger"><i class="fas fa-heart"></i> 59 </a> <a href="" class="btn btn-secondary"><i class="fas fa-heart-broken"></i> 5  </a> <a href="" class="btn btn-light"><i class="fas fa-comment-dots"></i> Comentario(s)</a>
                   </div>
                 </div>
                @endforeach
-           </div>
-           <div class="col-3" id="tendencias">
            </div>
         </div>
     </div>
